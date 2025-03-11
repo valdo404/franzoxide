76476d00db5a4d621a1099de7412cd387a9351e9
Laurent Valdes <laurent@lapoule.dev>
2025-03-11 19:21:02 +0100

    feat(project): add Rust project configuration files

---
+++ build.rs
@@ -0,0 +1,12 @@
+// Removed unused imports
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Tell Cargo to re-run this build script if the proto file changes
+    println!("cargo:rerun-if-changed=proto/connector.proto");
+    println!("cargo:rerun-if-changed=proto");
+    
+    // Compile the proto file
+    tonic_build::compile_protos("proto/connector.proto")?;
+    
+    Ok(())
+}
